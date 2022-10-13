64e0db3fb8368fe8fb65863dd9bf048ffff70fef
Julian01 <iuliancrisan01@gmail.com>
2022-10-13 15:54:17 +0300

    Implemented consuming pandaflag api

--- index.ts
+++ index.ts
@@ -1,3 +1,58 @@
-export interface Test {
-  value: string
+import { io, Socket } from 'socket.io-client'
+
+const PANDAFLAG_SOCKET_URL: string = 'ws://api.pandaflag.com/'
+
+export interface Flag {
+  id: string
+  name: string
+  description: string
+  enabled: boolean
 }
+
+class PandaflagClient {
+  private flags: Flag[] = []
+  private isLoading: boolean = true
+
+  init(apiKey: string, environment: string) {
+    const socket: Socket = io(PANDAFLAG_SOCKET_URL, { query: { apiKey, environment } })
+
+    socket.on('connect', () => {
+      socket.on('newFlags', (data: Flag[]) => {
+        this.flags = data
+        this.isLoading = false
+      })
+
+      socket.on('error', (data: Error) => {
+        socket.disconnect()
+        throw new Error(data.message)
+      })
+    })
+  }
+
+  getFlags() {
+    return this.flags
+  }
+
+  getFlag(flagName: string) {
+    if (!flagName) {
+      throw new Error('Flag name unspecified')
+    }
+
+    const foundFlag = this.flags.find((flag: Flag) => flag.name === flagName)
+    if (this.isLoading || !foundFlag) {
+      return undefined
+    }
+
+    return foundFlag
+  }
+}
+
+export default new PandaflagClient()
+
+// const client = new PandaflagClient()
+// client.init('1e9743f9-e32d-4c7d-9663-09ba4ef3627d', 'production')
+
+// setTimeout(() => {
+//   console.log('Flags')
+//   console.log(client.getFlags())
+// }, 3000)
